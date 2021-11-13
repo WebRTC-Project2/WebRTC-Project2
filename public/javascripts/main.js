eab76af6b057e929d30744629062027dae2bb0e4
jbaranda005 <jbaranda005@gmail.com>
2021-11-13 12:09:09 -0600

    INitialization of music player and events added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -192,6 +192,40 @@ function appendMessage (sender, message){
 
 }
 
+//Initialization of Web Playback SDK for Spotify
+window.onSpotifyWebPlaybackSDKReady = () => {
+  const token = 'xxxxxxx'; //WE have to add a token of a premium spotify account
+  const player = new Spotify.Player({
+    name: 'Web Playback SDK Quick Start Player',
+    getOAuthToken: cb => { cb(token); },
+    volume: 0.5
+  });
+
+  // Ready
+    player.addListener('ready', ({ device_id }) => {
+      console.log('Ready with Device ID', device_id);
+    });
+
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+      console.log('Device ID has gone offline', device_id);
+    });
+    player.addListener('initialization_error', ({ message }) => {
+     console.error(message);
+   });
+
+   player.addListener('authentication_error', ({ message }) => {
+       console.error(message);
+   });
+
+   player.addListener('account_error', ({ message }) => {
+       console.error(message);
+   });
+
+   //Connect to Spotify
+   player.connect();
+
+
 
 /* WebRTC Events */
 
