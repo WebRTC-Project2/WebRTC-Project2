c9411d73e2bccf968866da878d9a81ebb7a09bea
mzahid3 <naveedzahid94@yahoo.com>
2021-11-16 21:18:53 -0600

    Added the code from other people to fix the conflict

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -29,6 +29,10 @@ registerScEvents();
 
 /* DOM Elements */
 
+const filesForm = document.querySelector('#files-form');
+
+filesForm.addEventListener('submit', handleFilesForm);
+
   const button = document
     .querySelector('#call-button');
 
@@ -50,6 +54,87 @@ registerScEvents();
 
   vidButton.addEventListener('click',
    stopVid);
+
+    function handleFilesForm(event){
+     event.preventDefault();
+     const form = event.target;
+     const fileInput = form.querySelector('#files-input');
+     const file = fileInput.files[0];
+     console.log('Got a file with the name', file.name);
+     sendFile($peer,file);
+    }
+
+    function sendFile(peer, file) {
+     // create a package of file metadata
+     const metadata = {
+       name: file.name,
+       size: file.size,
+       type: file.type
+     };
+     const chunk = 8 * 1024; // 8KiB (kibibyte)
+     // console.log(JSON.stringify(metadata));
+     // create an asymmetric data channel
+     const fdc = peer.connection
+       .createDataChannel(`file-${metadata.name}`);
+
+
+     if (
+       !$peer.features ||
+       ($self.features.binaryType !== $peer.features.binaryType)
+     ) {
+       fdc.binaryType = 'arraybuffer';
+     }
+     console.log(`Lets use the ${fdc.binaryType} data type!`);
+
+
+     fdc.onopen = async function() {
+       // send the metadata, once the data channel opens
+       console.log('Created a data channel with ID', fdc.id);
+       console.log('Heard datachannel open event.')
+       console.log('Use chunk size', chunk);
+       fdc.send(JSON.stringify(metadata));
+
+       // send the actual file data
+       let data =
+         fdc.binaryType === 'blob' ? file : await file.arrayBuffer();
+         console.log(metadata.size);
+       for(let i = 0; i < metadata.size; i += chunk) {
+         console.log('Attempting to send a chunk of data...');
+         fdc.send(data.slice(i, i + chunk));
+       }
+
+     };
+     fdc.onmessage = function({ data }) {
+       // handle an acknowledgement from the receiving peer
+       let message = JSON.parse(data);
+       console.log('Successfully sent file', message.name);
+       console.log('Closing the data channel');
+       fdc.close();
+     }
+   }
+
+    function receivedFile(fdc){
+       const chunk = [];
+       let receivedBytes = 0;
+       let metadata;
+       fdc.onmessage = function({data}){
+         let message = data;
+         if (typeof(message) === 'string' && message.startsWith('{')){
+           metadata = JSON.parse(message);
+           console.log(`received metadata ${message}`)
+         }
+         else{
+           console.log('Received file data')
+           chunk.push(data);
+           receivedBytes += data.size ? data.size : data.byteLength;
+           console.log('Received bytes so far', receivedBytes);
+         }
+         if (receivedBytes === metadata.size){
+           console.log('File transfer complete');
+         }
+       }
+
+     }
 
 
   const audbutton = document
@@ -447,3 +532,26 @@ async function handleScSignal({ from, signal: { description, candidate } }) {
    if (set_location) window.location.hash = ns;
    return ns;
  }
+
+function showSpotifyPlayer(url) {
+  let node = document.getElementById("spotify-iframe");
+  node.innerHTML = `<iframe src="${url}" id="spotify-player" style="width:100%;bottom:0;" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>`;
+}
+function uploadSpotifyUrl() {
+  let song_url = document.getElementById("song-url").value;
+  if (!song_url.includes("https://open.spotify.com/")) {
+    alert("not a valid spotify embed url");
+    return false;
+  }
+  song_url=song_url.replace(".com/",".com/embed/");
+  showSpotifyPlayer(song_url);
+  if (sc.connected) {
+    console.log("sending the song url");
+    sc.emit('uploadsong',
+      {
+        url: song_url,
+        namespace: namespace
+      });
+  }
+  return false;
+}
